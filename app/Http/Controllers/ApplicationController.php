1fdb8c67408bb15258ff602815d50a0ad51f798d
umidbeksalayev <umidbeksalayev13@gmail.com>
2026-03-13 22:43:05 +0500

    the manager and client pages worked.

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Application;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    //
+
+    public function store(Request $request){
+        if($request->hasFile('file')){
+            $name = $request->file('file')->getClientOriginalName();
+           $path = $request->file('file')->storeAs(
+               'files',
+               $name,
+               'public'
+           );
+        }
+        $request->validate([
+            'subject' => 'required |max:255',
+            'message' => 'required',
+            'file' => 'file|mimes:jpeg,png,jpg,pdf,gif,svg|max:2048'
+        ]);
+       $application = Application::create([
+           'user_id'=>auth()->user()->id,
+           'subject'=>$request->subject,
+           'message'=>$request->message,
+           'file_url'=>$path ?? null,
+       ]);
+
+       return redirect()->back();
+    }
 }
