3743cd0714cdd2ad78448f35b4ceb30c9f84da2c
umidbeksalayev <umidbeksalayev13@gmail.com>
2026-03-21 15:49:29 +0500

    creating only 1 application a day is done

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
 
     public function store(Request $request){
+
+        $this->checkDate();
+
+        if($this->checkDate()){
+          return  redirect()->back()->with('error','Your can create only 1 day application a day');
+        }
         if($request->hasFile('file')){
             $name = $request->file('file')->getClientOriginalName();
            $path = $request->file('file')->storeAs(
@@ -30,5 +37,18 @@ class ApplicationController extends Controller
        ]);
 
        return redirect()->back();
+    }
+
+    protected function checkDate(){
+        if(auth()->user()->applications()->latest()->first() == null){
+            return false;
+        }
+        $last_application = auth()->user()->applications()->latest()->first();
+        $last_app_date = Carbon::parse($last_application->created_at)->format('Y-m-d');
+        $today = Carbon::now()->format('Y-m-d');
+
+        if($last_app_date == $today){
+            return true;
+        }
     }
 }
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -12,6 +13,9 @@ class MainController extends Controller
     }
     public function dashboard()
     {
+//        $last_application = auth()->user()->applications()->latest()->first();
+//        $last_app_date = Carbon::parse($last_application->created_at)->format('Y-m-d');
+//        $today = Carbon::now()->format('Y-m-d');
         return view('dashboard')->with([
             'applications'=>Application::latest()->paginate(4),
         ]);
